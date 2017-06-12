b0704b8e43b9dfac6a4794c781212b416d43911c
megri <teknocide@gmail.com>
2017-06-13 00:04:10 +0200

    fix build

--- build/build.scala
+++ build/build.scala
@@ -1,7 +1,7 @@
 package cbt_test_build
 import cbt._
 
-class Build(val context: Context) extends BaseBuild{
+class Build(val context: Context) extends Macroparadise{
   override def defaultScalaVersion = "2.12.2"
   
   override def dependencies = 
